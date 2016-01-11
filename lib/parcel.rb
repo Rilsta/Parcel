d77a658ab430068796fdbd58497d431ddcaa0522
Gregory K and Riley Starnes <rstarnes9@gmail.com>
2016-01-11 11:43:13 -0800

    Add tests

--- lib/parcel.rb
+++ lib/parcel.rb
@@ -4,11 +4,25 @@ class Parcel
     @width = width
     @height = height
     @weight = weight
+    @volume = @length * @width * @height
   end
 
-  def volume()
-    vol = @length * @width * @height
-    return vol
+  def volume_cost()
+    @vol_cost = (@volume * 2.00)
+    return @vol_cost
   end
 
+  def weight_cost()
+    @weigh_cost = (@weight * 1.00)
+  end
+
+  def shipping(mi)
+    @shipping_cost = (0.20 * mi)
+    return @shipping_cost
+  end
+
+  def cost_to_ship()
+    cost = @vol_cost + @weight_cost + @shipping_cost
+    return cost
+  end
 end
--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -2,10 +2,24 @@ require('rspec')
 require('parcel')
 
 describe(Parcel) do
-  describe('#volume') do
-    it('calculates the volume of a package') do
+  describe('#volume_cost') do
+    it('calculates the volume cost of a package') do
       package = Parcel.new(2,2,2,34)
-      expect(package.volume()).to(eq(8))
+      expect(package.volume_cost()).to(eq(16))
+    end
+  end
+
+  describe('#weight_cost') do
+    it('calculates weight cost of a package') do
+      package = Parcel.new(2,2,2,34)
+      expect(package.weight_cost()).to(eq(34))
+    end
+  end
+
+  describe('#shipping') do
+    it('calculates shipping cost of a package') do
+      package = Parcel.new(2,2,2,34)
+      expect(package.shipping(10)).to(eq(2))
     end
   end
 end
